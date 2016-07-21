fd047e3c11291e2e19700661c32587768c6bab13
Mahati Gollamudi <mahatigollamudi@gmail.com>
2016-07-21 19:42:31 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
+nums=[1,2,3]
 def my_select(collection)
- # your code here!
+  new_array = []
+  i = 0
+    while i < collection.length
+      myBoolean = yield collection[i]
+      if myBoolean == true
+        new_array.push(collection[i])
+        i+=1
+      else
+        i+=1
+      end
+    end
+  new_array
 end
